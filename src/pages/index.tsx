870dacabc08a0ab9881e98ad3ccd762473330a78
Maxim Murphy <maxim.murphy8@gmail.com>
2023-03-31 12:24:45 -0400

    added react hot toast lib

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,14 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
-import { Loader } from "@/components/Loader";
+import toast from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <div>
-      <Loader show />
+      <button onClick={() => toast.success("hello toast!")}>Toast Me</button>
     </div>
   );
 }
